2653b9da09d9c0f5654426ae650b653bfdd1c829
REEMAALHUDAIAN <reemaalhudaian@gmail.com>
2024-10-27 18:34:02 +0300

    Initial Commit

---
+++ recipee/recipeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeeApp.swift
+//  recipee
+//
+//  Created by Reema ALhudaian on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct recipeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipeeTests/recipeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipeeTests.swift
+//  recipeeTests
+//
+//  Created by Reema ALhudaian on 24/04/1446 AH.
+//
+
+import Testing
+@testable import recipee
+
+struct recipeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
